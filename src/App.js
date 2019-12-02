52b3ba2175c6ba6d6ea724f9a0e7e31d853dde7e
Mayur Panchal <mayurpanchal002@gmail.com>
2019-12-02 22:34:32 +0530

    css classes added for initial design

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ import Header from './components/Header';
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Header />
-			<div>
-				<Route path='/' exact component={StreamList} />
-				<Route path='/streams/new' exact component={StreamCreate} />
-				<Route path='/streams/edit' exact component={StreamEdit} />
-				<Route path='/streams/delete' exact component={StreamDelete} />
-				<Route path='/streams/show' exact component={StreamShow} />
-			</div>
-		</BrowserRouter>
+		<div className='ui container'>
+			<BrowserRouter>
+				<Header />
+				<div>
+					<Route path='/' exact component={StreamList} />
+					<Route path='/streams/new' exact component={StreamCreate} />
+					<Route path='/streams/edit' exact component={StreamEdit} />
+					<Route path='/streams/delete' exact component={StreamDelete} />
+					<Route path='/streams/show' exact component={StreamShow} />
+				</div>
+			</BrowserRouter>
+		</div>
 	);
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-	return <div>Header comp</div>;
+	return (
+		<div className='ui secondary pointing menu'>
+			<Link className='item' to='/'>
+				Streamy
+			</Link>
+			<div className='right menu'>
+				<Link className='item' to='/'>
+					All Streams
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
