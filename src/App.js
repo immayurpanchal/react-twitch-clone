cb7b4ebce9511e74eeaa4ea561c4b288eae0f20c
Mayur Panchal <mayurpanchal002@gmail.com>
2019-12-02 20:44:07 +0530

    imported all streams in App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-	return (
-		<div>
-			PageOne
-			{/* using <a> is a Bad !! Approach */}
-			<Link to='/pageTwo'>Go to Page 2</Link>
-		</div>
-	);
-};
-
-const PageTwo = () => {
-	return (
-		<div>
-			PageTwo
-			{/* using <a> is a Bad !! Approach */}
-			<Link to='/'>Go to Page 1</Link>
-		</div>
-	);
-};
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './components/streams/StreamList';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamShow from './components/streams/StreamShow';
 
 const App = () => {
 	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Route path='/' exact component={PageOne} />
-					<Route path='/pageTwo' component={PageTwo} />
-				</div>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<div>
+				<Route path='/' exact component={StreamList} />
+				<Route path='/streams/new' exact component={StreamCreate} />
+				<Route path='/streams/edit' exact component={StreamEdit} />
+				<Route path='/streams/delete' exact component={StreamDelete} />
+				<Route path='/streams/show' exact component={StreamShow} />
+			</div>
+		</BrowserRouter>
 	);
 };
 
