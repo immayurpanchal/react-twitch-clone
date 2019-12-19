710624a5cde8821f14a3a6f1789edebfbb79ab36
Mayur Panchal <mayur.panchal@vuclip.com>
2019-12-19 20:56:54 +0530

    Added support for redux form in react

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,25 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import { reducer as FormReducer } from "redux-form";
 
 const initialState = {
-	profile : {
-		email    : '',
-		name     : '',
-		imageUrl : ''
-	}
+  profile: {
+    email: "",
+    name: "",
+    imageUrl: ""
+  }
 };
 
 const profileReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'SUCCESS':
-			const { email, name, imageUrl } = action.payload;
-			return { ...state, email, name, imageUrl };
-		default:
-			return state.profile;
-	}
+  switch (action.type) {
+    case "SUCCESS":
+      const { email, name, imageUrl } = action.payload;
+      return { ...state, email, name, imageUrl };
+    default:
+      return state;
+  }
 };
 
 export default combineReducers({
-	profile : profileReducer
+  profile: profileReducer,
+  form: FormReducer
 });
