24d71e36ae1e3c33dc4a3f5486eade23bc6892e3
Mayur Panchal <mayurpanchal002@gmail.com>
2019-12-25 15:06:30 +0530

    googleId Added

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,51 +1,50 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
-import { signIn, signOut } from "../actions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
-  responseGoogle = res => {
-    const { email, name, imageUrl } = res.profileObj;
-    this.props.setProfile(email, name, imageUrl);
-  };
+	responseGoogle = (res) => {
+		const { email, name, imageUrl, googleId } = res.profileObj;
+		this.props.setProfile(email, name, imageUrl, googleId);
+	};
 
-  responseGoogleLogout = () => {
-    this.props.removeProfile();
-  };
+	responseGoogleLogout = () => {
+		this.props.removeProfile();
+	};
 
-  render() {
-    return (
-      <div>
-        {this.props.isSignedIn && (
-          <GoogleLogout
-            clientId="883748642736-e3f7grf3ibrhu3d4ikb971vp5ta01t02.apps.googleusercontent.com"
-            buttonText="Logout"
-            onLogoutSuccess={this.responseGoogleLogout}
-          />
-        )}
-        {!this.props.isSignedIn && (
-          <GoogleLogin
-            clientId="883748642736-e3f7grf3ibrhu3d4ikb971vp5ta01t02.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={this.responseGoogle}
-            cookiePolicy={"single_host_origin"}
-          />
-        )}
-      </div>
-    );
-  }
+	render () {
+		return (
+			<div>
+				{this.props.isSignedIn && (
+					<GoogleLogout
+						clientId='883748642736-e3f7grf3ibrhu3d4ikb971vp5ta01t02.apps.googleusercontent.com'
+						buttonText='Logout'
+						onLogoutSuccess={this.responseGoogleLogout}
+					/>
+				)}
+				{!this.props.isSignedIn && (
+					<GoogleLogin
+						clientId='883748642736-e3f7grf3ibrhu3d4ikb971vp5ta01t02.apps.googleusercontent.com'
+						buttonText='Login'
+						onSuccess={this.responseGoogle}
+						cookiePolicy={'single_host_origin'}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return { isSignedIn: state.profile.email };
+const mapStateToProps = (state) => {
+	return { isSignedIn: state.profile.email };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setProfile: (email, name, imageUrl) =>
-      dispatch(signIn({ email, name, imageUrl })),
-    removeProfile: () => dispatch(signOut())
-  };
+const mapDispatchToProps = (dispatch) => {
+	return {
+		setProfile    : (email, name, imageUrl, googleId) => dispatch(signIn({ email, name, imageUrl, googleId })),
+		removeProfile : () => dispatch(signOut())
+	};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(GoogleAuth);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,34 @@
-import { combineReducers } from "redux";
-import { reducer as FormReducer } from "redux-form";
-import { SIGN_IN, SIGN_OUT } from "../actions/types";
+import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form';
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const initialState = {
-  profile: {
-    email: "",
-    name: "",
-    imageUrl: ""
-  }
+	profile : {
+		email    : '',
+		name     : '',
+		imageUrl : '',
+		googleId : ''
+	}
 };
 
 const profileReducer = (state = initialState.profile, action) => {
-  switch (action.type) {
-    case SIGN_IN:
-      const { email, name, imageUrl } = action.payload;
-      return { email, name, imageUrl };
-    case SIGN_OUT:
-      return { email: "", name: "", imageUrl: "" };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SIGN_IN:
+			const { email, name, imageUrl, googleId } = action.payload;
+			return {
+				email,
+				name,
+				imageUrl,
+				googleId
+			};
+		case SIGN_OUT:
+			return { email: '', name: '', imageUrl: '' };
+		default:
+			return state;
+	}
 };
 
 export default combineReducers({
-  profile: profileReducer,
-  form: FormReducer
+	profile : profileReducer,
+	form    : FormReducer
 });
